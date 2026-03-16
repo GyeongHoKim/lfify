02b77d025199e292143f2b0dc2644ee8e761b519
GyeongHo Kim <gyeongho.dev@proton.me>
2026-03-16 20:33:46 +0900

    test: add e2e tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,8 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  { ignores: ["**/__fixtures__/**"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   {
     languageOptions: {
       globals: {
